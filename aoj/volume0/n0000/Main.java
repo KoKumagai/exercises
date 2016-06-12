b9ae858e59ce65c9a0268718fd56e6750938b2ba
Ko Kumagai <ko.kumagai.ko@gmail.com>
2016-06-11 21:33:07 -0400

    Create 0000

---
+++ aoj/volume0/n0000/Main.java
@@ -0,0 +1,17 @@
+package aoj.volume0.n0000;
+
+public class Main {
+
+    public static void main(String[] a) {
+
+        for (int i = 1; i <= 9; i++) {
+
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + "x" + j + "=" + i * j);
+            }
+
+        }
+
+    }
+
+}
