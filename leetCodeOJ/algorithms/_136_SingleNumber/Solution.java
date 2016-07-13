9f7afd3e341e19bb5be692b72d705101c00bafe8
Ko Kumagai <ko.kumagai.ko@gmail.com>
2016-07-13 15:27:51 -0400

    Create : 136 Single Number

---
+++ leetCodeOJ/algorithms/_136_SingleNumber/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public int singleNumber(int[] nums) {
+
+        int singleNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+		        singleNum ^= nums[i];
+
+        }
+
+	      return singleNum;
+
+    }
+
+}
