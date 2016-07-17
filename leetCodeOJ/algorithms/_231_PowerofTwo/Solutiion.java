8f6987cb650272c8774be4e8a65288914d54de6c
Ko Kumagai <ko.kumagai.ko@gmail.com>
2016-07-17 16:32:08 -0400

    Create : 231 Power of Two

---
+++ leetCodeOJ/algorithms/_231_PowerofTwo/Solutiion.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    public boolean isPowerOfTwo(int n) {
+
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n != 1) {
+
+            if ( n % 2 == 0) {
+                n /= 2;
+            } else {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+
+}
