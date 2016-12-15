c34803dc8a7257028c755f7f23cfee9a9e48cda6
Ko Kumagai <ko.kumagai.ko@gmail.com>
2016-12-16 02:06:06 +0900

    272 : NOT回路

---
+++ yukicoder/272_NOT回路/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+
+        int not = 1 - X;
+
+        System.out.println(not);
+
+    }
+
+}
