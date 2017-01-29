6fb97729416f75befe1ca23520cf42f29db413ef
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-01-29 16:30:38 +0900

    476 : 正しくない平均

---
+++ yukicoder/476_正しくない平均/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        long answer = sc.nextInt();
+
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+
+        System.out.println(sum == answer * n ? "YES" : "NO");
+
+    }
+}
