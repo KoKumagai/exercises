478367ef66a3c58f49a244d9d0c4def6047c8d4d
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-04-24 18:08:58 +0900

    80 : 四角形を描こう

---
+++ yukicoder/80_四角形を描こう/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int D = sc.nextInt();
+
+        System.out.println((D / 4) * ((D + 2) / 4));
+
+    }
+
+}
