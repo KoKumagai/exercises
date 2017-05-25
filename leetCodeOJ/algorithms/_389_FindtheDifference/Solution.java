5ca67016c82de626613f59cc892593f3bb316ac0
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-05-25 09:16:15 +0900

    Create : 389 Find the Difference

--- leetCodeOJ/algorithms/_389_FindtheDifference/Solution.java
+++ leetCodeOJ/algorithms/_389_FindtheDifference/Solution.java
@@ -4,11 +4,11 @@ public class Solution {
 
         char addedChar = 0;
 
-        for (int i = 0; i < s.length(); ++i) {
+        for (int i = 0; i < s.length(); i++) {
             addedChar ^= s.charAt(i);
         }
 
-        for (int i = 0; i < t.length(); ++i) {
+        for (int i = 0; i < t.length(); i++) {
             addedChar ^= t.charAt(i);
         }
 
