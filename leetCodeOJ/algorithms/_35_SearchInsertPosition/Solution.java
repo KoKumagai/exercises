be8791cf4cb82187294316aa5442edd04edf4158
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-06-19 05:07:43 +0900

    Create : 35 Search Insert Position

---
+++ leetCodeOJ/algorithms/_35_SearchInsertPosition/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+
+    public static int searchInsert(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+
+        }
+
+        return low;
+
+    }
+
+}
