c639ab8446b56c7b3cc6c9dd9c2c333d6fff08d2
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-06-25 21:15:13 +0900

    Create : 367 Valid Perfect Square

---
+++ leetCodeOJ/algorithms/_367_ValidPerfectSquare/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+    public boolean isPerfectSquare(int num) {
+
+        for (int i = 1; num > 0; i += 2) {
+            num -= i;
+        }
+
+        return num == 0;
+
+    }
+
+}
