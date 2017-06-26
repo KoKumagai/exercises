e4c8ef8eeb7fddc0c89347a0c989c750902ffe87
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-06-26 21:21:25 +0900

    530 : 年齢って毎年変わるし覚えるの難しいよね

---
+++ yukicoder/530_年齢って毎年変わるし覚えるの難しいよね/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int BIRTH_YEAR = sc.nextInt();
+
+        System.out.println(2017 - BIRTH_YEAR);
+
+    }
+
+}
