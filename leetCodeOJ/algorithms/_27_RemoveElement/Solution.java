efac8038fe9d9206a103350275b37a40bad627c4
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-07-04 22:40:24 +0900

    Create : 27 Remove Element

---
+++ leetCodeOJ/algorithms/_27_RemoveElement/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+    public int removeElement(int[] nums, int val) {
+
+        int lenCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != val) {
+                nums[lenCount] = nums[i];
+                lenCount++;
+            }
+
+        }
+
+        return lenCount;
+
+    }
+
+}
