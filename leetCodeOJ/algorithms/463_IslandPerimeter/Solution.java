df0430ccda324234acae1611a66ead175b5ecde5
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-08-20 16:06:09 +0900

    Create : 463 Island Perimeter

---
+++ leetCodeOJ/algorithms/463_IslandPerimeter/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+
+    public int islandPerimeter(int[][] grid) {
+
+        int perimeter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+
+            for (int j = 0; j < grid[i].length; j++) {
+
+                if (grid[i][j] == 1) {
+
+                    perimeter += 4;
+
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+
+                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) {
+                        perimeter -= 2;
+                    }
+
+                }
+
+            }
+
+        }
+
+        return perimeter;
+
+    }
+
+}
