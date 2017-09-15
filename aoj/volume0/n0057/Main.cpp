b8d58f8b3913dcb61c207cf1e2005f0e0d2fb0e9
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-09-16 00:58:27 +0900

    Create : 0057

---
+++ aoj/volume0/n0057/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num;
+    while (cin >> num) {
+
+        cout << num * (num + 1) / 2 + 1 << endl;
+
+    }
+
+    return 0;
+
+}
