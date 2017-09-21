43265d1c06b0b0954214a85870e0bade4a769913
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-09-21 15:58:03 +0900

    Create : 0094

---
+++ aoj/volume0/n0094/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+
+    double area = a * b / 3.305785;
+
+    printf("%.6lf\n", area);
+
+    return 0;
+
+}
