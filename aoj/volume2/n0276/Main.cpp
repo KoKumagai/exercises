de3997495c59f779e943a1a939193148d2e11b58
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-10-17 00:28:05 +0900

    Create : 0276

---
+++ aoj/volume2/n0276/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    for (int i = 1; i <= 7; i++) {
+
+        int highest, lowest;
+        cin >> highest >> lowest;
+
+        cout << (highest - lowest) << endl;
+
+    }
+
+    return 0;
+
+}
