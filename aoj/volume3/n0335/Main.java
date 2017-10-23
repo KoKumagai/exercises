9bfed7025b337d75cfccc4ebde5f55c6e6f65c7c
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-10-24 02:02:45 +0900

    Create : 0335

---
+++ aoj/volume3/n0335/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int word = sc.nextInt();
+
+        System.out.println(word * 32);
+
+    }
+
+}
