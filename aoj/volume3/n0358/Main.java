67f5441358563f40a89f9b525280f2f8810d2f10
Ko Kumagai <ko.kumagai.ko@gmail.com>
2017-11-04 09:17:17 +0900

    Create : 0358

---
+++ aoj/volume3/n0358/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int f = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (m >= b) {
+            System.out.println(0);
+        } else if (m + f >= b) {
+            System.out.println(b - m);
+        } else {
+            System.out.println("NA");
+        }
+
+    }
+
+}
