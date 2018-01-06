d9e50cad7ac1e79f76157de7473c72ac23aec155
Ko Kumagai <ko.kumagai.ko@gmail.com>
2018-01-07 08:02:21 +0900

    Create : 383 レーティング

---
+++ yukicoder/383_レーティング/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+
+    if (a < b) {
+        cout << "+";
+    }
+
+    cout << b - a << endl;
+
+    return 0;
+
+}
