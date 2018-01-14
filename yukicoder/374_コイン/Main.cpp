47acc6e2ecfa0d28fec3ee147dc42a226d42cfd4
Ko Kumagai <ko.kumagai.ko@gmail.com>
2018-01-14 19:33:02 +0900

    Create : 374 コイン

---
+++ yukicoder/374_コイン/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    long long A, B;
+    cin >> A >> B;
+
+    if (A < B) {
+        cout << "K" << endl;
+    } else {
+        cout << "S" << endl;
+    }
+
+    return 0;
+
+}
