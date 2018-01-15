256b226d3e094f9f67352d75619b392a168eb4d1
Ko Kumagai <ko.kumagai.ko@gmail.com>
2018-01-15 19:46:56 +0900

    Create : isPrime2

---
+++ etc/isPrime/IsPrime2.java
@@ -0,0 +1,41 @@
+public class IsPrime2 {
+
+    private static boolean isPrime(int num) {
+
+        if (num <= 1) {
+            return false;
+        }
+
+        if (num == 2) {
+            return true;
+        }
+
+        if (num % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i * i <= num; i += 2) {
+
+            if (num % i == 0) {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+}
